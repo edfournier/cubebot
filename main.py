1422b8a2241b4d413174563a3d872ac6dcebf8c0
edfournier <ericfournier.d@gmail.com>
2023-12-06 20:37:01 -0500

    fixed main

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import sys
 import cv2
 import serial
 sys.path.append("sensing")
+
 sys.path.append("solving")
+from time import sleep
 from sensor import Sensor
 from solver import solveCube
 
-PORT = ""
-BAUD_RATE = 0
+PORT = "/dev/cu.usbserial-10"
+BAUD_RATE = 115200
 
 print("Sensing scramble...")
 
+socket = serial.Serial(PORT, BAUD_RATE)
 sensor = Sensor()
-rotations = ['x', 'y', 'y', 'y', 'yx', 'xx']    # x is up move, y is right
+rotations = ["x", "y", "y", "y", "yx", "xx"]    # x is up move, y is right
 offsets = [0, 8, 4, 20, 16, 12]                 # read top, then front
 scramble = [0] * 24
  
@@ -23,16 +26,23 @@ for i in range(6):
     print(side)
     for j in range(4):
         scramble[offsets[i] + j] = side[j]
-    # send rotations[i] to ESP32 and wait to proceed
 
 sensor.release()
 
 print("Scramble:", scramble)
 print("Solving scramble....")
 
-solution = solveCube(scramble)[0]
+solution = solveCube(scramble)[0]   \
+            .replace("F2", "F F")   \
+            .replace("U2", "U U")   \
+            .replace("R2", "R R")   \
+            .replace("F'", "f")     \
+            .replace("U'", "u")     \
+            .replace("R'", "r")     \
 
 print("Solution:", solution)
-print("Sending solution to ESP32...")
+print("Ready to send solution")
+input()
 
-# send solution to EPS32
+socket.write(solution.encode())
+socket.close()
